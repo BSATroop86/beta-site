5f2fd38a056bf73eb1c79c0f457f14860a0d2a19
Chris Bentivenga <chris@chrisbenti.com>
2022-06-16 17:40:05 +0800

    some more pages converted

--- .old/readme.md
+++
@@ -1,1 +0,0 @@
-# [https://www.bsatroop86.org/](https://www.bsatroop86.org/)
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,5 +1,5 @@
 import { MarkdownPage } from "./components/MarkdownPage";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 
 import About from "./pages/about.md";
 import Contact from "./pages/contact.md";
@@ -9,6 +9,7 @@ import EagleResources from "./pages/eagleResources.md";
 import NewScouts from "./pages/newScoutResources.md";
 import { HomePage } from "./pages/HomePage";
 import { EaglesPage } from "./pages/EaglesPage";
+import { RedirectPage } from "./pages/RedirectPage";
 
 const MarkdownPaths = [
   { path: "/about", md: About, title: "About" },
@@ -17,6 +18,13 @@ const MarkdownPaths = [
   { path: "/summerCamp", md: SummerCamp, title: "Summer Camp" },
   { path: "/eagleResources", md: EagleResources, title: "Eagle Resources" },
   { path: "/newScoutResources", md: NewScouts, title: "New Scouts" },
+];
+
+const Redirects = [
+  {
+    path: "firefighters",
+    to: "https://www.google.com/maps/d/viewer?mid=1fkmvLVslcM5LqTT_uSoEeZ91Egs",
+  },
 ];
 
 export const T86Routes = () => (
@@ -28,6 +36,9 @@ export const T86Routes = () => (
         path={path}
         element={<MarkdownPage title={title} location={md} />}
       />
+    ))}
+    {Redirects.map(({ path, to }) => (
+      <Route path={path} element={<RedirectPage href={to} />} />
     ))}
   </Routes>
 );
